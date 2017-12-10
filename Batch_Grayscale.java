11143db71e2886f0af5e05631225e5120b496ab2
Jinlin Song <jlsong@ucdavis.edu>
2017-12-09 18:24:10 -0800

    add two java files

--- Batch_Grayscale.java
+++ Batch_Grayscale.java
@@ -1,20 +1,23 @@
+
 /**
- * Image Inversion
+ * convert color images to gray-scale images
  * 
  * Jinlin
  * 12/19/2017 18:17:00 PST
  */
+
 import edu.duke.*;
 import java.io.*;
 
-public class Image_Inversion {
-    public ImageResource makeInverse(ImageResource inImage) {
+public class Batch_Grayscale {
+    public ImageResource makeGray(ImageResource inImage) {
         ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
         for (Pixel pixel: outImage.pixels()) {
             Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-            pixel.setRed(255 - inPixel.getRed());
-            pixel.setGreen(255 - inPixel.getBlue());
-            pixel.setBlue(255 - inPixel.getGreen());
+            int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen())/3;
+            pixel.setRed(average);
+            pixel.setGreen(average);
+            pixel.setBlue(average);
         }
         return outImage;
     }
@@ -24,8 +27,8 @@ public class Image_Inversion {
         for (File f : dr.selectedFiles()) {
             ImageResource inImage = new ImageResource(f);
             String fname = inImage.getFileName();
-            ImageResource gray = makeInverse(inImage);
-            String newName = "images/inverted-" + fname;
+            ImageResource gray = makeGray(inImage);
+            String newName = "images/gray_" + fname;
             gray.setFileName(newName);
             gray.save();
         }
